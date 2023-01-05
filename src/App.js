62fbc22d25f4a990a995b522f0e284499000380a
ttc <ttc@ethinking.de>
2023-01-05 09:52:44 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Carousel from 'react-elastic-carousel'
 import {WeatherProvider} from "./FetchApi.jsx";
 import DisplayWeatherWidget from "./DisplayWeatherWidget.jsx";
 import WidgetPage2 from "./WidgetPage2.jsx";
@@ -8,10 +7,10 @@ export default function App () {
 
     return (
         <WeatherProvider>
-            <Carousel>                
+            
                 <DisplayWeatherWidget/>
                 <WidgetPage2/>
-            </Carousel>
+
         </WeatherProvider>
 
     )
